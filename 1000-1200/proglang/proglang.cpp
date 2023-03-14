5f6f6b58de73e132dddca19f126e008086fb878c
bariskiratt <110984138+bariskiratt@users.noreply.github.com>
2023-03-14 11:16:03 +0300

    Create proglang.cpp

---
+++ 1000-1200/proglang/proglang.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t{};
+	cin >> t;
+	for(int i{}; i < t ; ++i){
+	    int a{},b{},a1{},b1{},a2{},b2{};
+	    cin >> a >> b >> a1 >> b1 >> a2 >> b2;
+	    if(((a == a1) && (b == b1)) || ((a == b1) && (b == a1))) cout << 1 << endl;
+	    else if(((a == a2) && (b == b2)) || ((a == b2) && (b == a2))) cout << 2 << endl;
+	    else cout << 0 << endl;
+	}
+	return 0;
+}
